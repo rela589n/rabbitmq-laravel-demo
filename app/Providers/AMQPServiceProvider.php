76664421f5dbeeb3d3154369f30b4c2fd32328dc
Yevhen Sidelnyk <zsidelnik@gmail.com>
2020-10-29 18:13:13 +0200

    feat(provider): configuration for amqp connetion to use rabbit

--- app/Providers/AMQPServiceProvider.php
+++ app/Providers/AMQPServiceProvider.php
@@ -4,6 +4,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AbstractConnection;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
@@ -12,7 +13,31 @@ final class AMQPServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        $this->app->singleton(
+            AbstractConnection::class,
+            function () {
+                $config = config('queue.connections.rabbitmq.hosts.messaging');
 
+                return $this->app->make(
+                    AMQPStreamConnection::class,
+                    [
+                        'host' => $config['host'],
+                        'port' => $config['port'],
+                        'user' => $config['user'],
+                        'password' => $config['password'],
+                        'vhost' => $config['vhost'],
+                    ]
+                );
+            }
+        );
+
+        $this->app->bind(
+            AMQPChannel::class,
+            function () {
+                return $this->app->make(AbstractConnection::class)
+                    ->channel();
+            }
+        );
     }
 
     public function boot()
