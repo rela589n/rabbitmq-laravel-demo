83757e46e84c0668e3427e0a1208c38d69a148d1
Yevhen Sidelnyk <zsidelnik@gmail.com>
2020-10-29 18:21:48 +0200

    feat(commands): example of worker doing time consuming tasks + acknowledgements

---
+++ app/Console/Commands/RabbitWorker/AMQPPusher.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands\RabbitWorker;
+
+use Illuminate\Console\Command;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class AMQPPusher extends Command
+{
+    protected $signature = 'amqp:push';
+    protected $description = 'Command description';
+
+    private AMQPChannel $channel;
+
+    public function __construct(AMQPChannel $channel)
+    {
+        parent::__construct();
+        $this->channel = $channel;
+    }
+
+    public function __destruct()
+    {
+        $this->channel->close();
+    }
+
+    public function handle()
+    {
+        $this->channel->queue_declare(
+            'jobs',
+            false,
+            false,
+            false,
+            false
+        );
+
+        while ($keepPushing ?? true) {
+            $time = (int)$this->ask('How much time should it take?');
+            $message = new AMQPMessage(
+                json_encode(['time' => $time, 'message' => 'New job!'], JSON_THROW_ON_ERROR),
+                [
+                    'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT,
+                    'content-type' => 'application/json',
+                ]
+            );
+
+            $this->channel->basic_publish($message, '', 'jobs');
+            $this->comment('Job sent!');
+
+            $keepPushing = $this->confirm('Continue?');
+        }
+    }
+}
---
+++ app/Console/Commands/RabbitWorker/AMQPWorker.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands\RabbitWorker;
+
+use Illuminate\Console\Command;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class AMQPWorker extends Command
+{
+    protected $signature = 'amqp:work';
+    protected $description = 'Processes queue';
+
+    private AMQPChannel $channel;
+
+    public function __construct(AMQPChannel $channel)
+    {
+        parent::__construct();
+        $this->channel = $channel;
+    }
+
+    public function __destruct()
+    {
+        $this->channel->close();
+    }
+
+    public function handle()
+    {
+        $this->channel->queue_declare(
+            'jobs',
+            false,
+            false,
+            false,
+            false
+        );
+
+        $this->comment('Waiting for jobs...');
+
+        $this->channel->basic_qos(null, 1, null);
+        $this->channel->basic_consume(
+            'jobs',
+            '',
+            false,
+            false,
+            false,
+            false,
+            fn($msg) => $this->process($msg)
+        );
+
+        while ($this->channel->is_consuming()) {
+            $this->comment('consuming..');
+            $this->channel->wait();
+        }
+    }
+
+    private function process(AMQPMessage $message): void
+    {
+        $body = json_decode($message->getBody(), true);
+
+        $this->comment('Received: '.$body['message']);
+        $this->comment('Estimated time:'.$body['time']);
+
+        sleep($body['time']);
+        $message->ack();
+
+        $this->info('Done!');
+    }
+}
